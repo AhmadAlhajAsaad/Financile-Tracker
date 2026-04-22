43bac1b9b9d6caec955afa32d318e544484ec68d
Ahmad Alhaj Asaad <ahmadalhajasaad1982@gmail.com>
2026-04-22 13:49:25 +0200

    feat: add CSRF protection to the application and configure Flask app with template and static folders

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 from .models import db, User, Category, DEFAULT_CATEGORIES
 from config import config
 
@@ -11,6 +12,7 @@ login_manager.login_message = "Log eerst in om deze pagina te bekijken."
 login_manager.login_message_category = "warning"
 
 migrate = Migrate()
+csrf = CSRFProtect()
 
 
 @login_manager.user_loader
@@ -19,7 +21,7 @@ def load_user(user_id):
 
 
 def create_app(env=None):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, template_folder="../templates", static_folder="../static")
 
     env = env or os.environ.get("FLASK_ENV", "default")
     app.config.from_object(config[env])
@@ -31,6 +33,7 @@ def create_app(env=None):
     db.init_app(app)
     login_manager.init_app(app)
     migrate.init_app(app, db)
+    csrf.init_app(app)
 
     # Blueprints registreren
     from .auth.routes import auth_bp
